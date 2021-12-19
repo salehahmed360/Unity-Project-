1ddd284f041dce06f987db333d3acbbf13f3edb2
salehahmed360 <62897570+salehahmed360@users.noreply.github.com>
2021-12-19 23:05:14 +0000

    level 1 vertical door animated and working
    vertical door prefab is animated to open and close based on the trigger

---
+++ Challenge of the tempo/Assets/VerticalDoor.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalDoor : MonoBehaviour
+{
+    public GameObject parent; //using parent object to access its children to stop null pointer reference
+    private doorTrigger doorTrigger;
+    private Animator door;
+    void Start()
+    {
+        door = parent.transform.GetChild(0).GetComponent<Animator>();
+        doorTrigger = parent.transform.GetChild(3).GetComponent<doorTrigger>(); //accessing the last index which is the trigger and getting the doorTrigger script
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        doorOpenening();
+        doorClosing();
+    }
+    public void doorOpenening()
+    {
+        if (doorTrigger.doorStatues == true)
+        {
+            door.SetBool("isOpening", true); 
+
+        }
+    } 
+    public void doorClosing()
+    {
+        if (doorTrigger.doorStatues == false)
+        {
+            door.SetBool("isOpening", false); 
+        }
+    }
+
+}

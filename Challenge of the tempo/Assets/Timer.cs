bce9d5306a604e62bad5b22222f603e05cd9597d
salehahmed360 <62897570+salehahmed360@users.noreply.github.com>
2021-12-23 01:49:10 +0000

    Basic Timer implemented
    Basic timer has been implemented in level 1 and level 2 which once timer value reaches 0 resets to the first level.

---
+++ Challenge of the tempo/Assets/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+public class Timer : MonoBehaviour
+{
+    [SerializeField]
+    public float timevalue = 60;
+    public Text timerText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(timevalue > 0)
+        {
+            timevalue -= Time.deltaTime;
+        }
+        else
+        {
+            timevalue = 0;
+            SceneManager.LoadScene(0); //restart to level 1 or 2
+        }
+        DisplayTime(timevalue);
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        if(timeToDisplay < 0)
+        {
+            timeToDisplay = 0;
+        }
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);//round timer to minutes
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60); //round timer to seconds by using modula
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);//the format which the time will be displayed
+    }
+}
